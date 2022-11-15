56d4322808e1ad352860c82dde59bbf28f74a115
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2022-11-15 09:13:43 +0000

    linting and roxygen comments

--- R/common.R
+++ R/common.R
@@ -1,25 +1,32 @@
-# Variables ------
-package_name <- "scctemplate"
-
-# Functions ------------
-
-# Test for existence of directory; if directory does not exist, attempt to recursively create directories to path
+#' Create directory
+#'
+#' Test for existence of directory; if directory does not exist,
+#' attempt to recursively create directories to path
+#'
+#' @param path path to create directory in
 create_directory <- function(path) {
   utils::file_test("-d", path) || dir.create(path, recursive = TRUE)
 }
 
-# Convert paths into the canonical form for the platform; on Windows this is "C:/Parent/Child"
-# A simple wrapper for the normalizePath() function
+#' Convert path
+#'
+#' Convert paths into the canonical form for the platform;
+#' on Windows this is "C:/Parent/Child"
+#' A simple wrapper for the normalizePath() function
+#'
+#' @param path path to normalise
 normalise_path <- function(path) {
   normalizePath(path = path, winslash = "/", mustWork = FALSE)
 }
 
-# Print package version to console
+#' Print version of the package
+#'
+#' Prints the version and name of this package.
 print_version <- function() {
-  
+  package_name <- "scctemplate"
   date <- as.character(utils::packageDate(pkg = package_name))
   version <- as.character(utils::packageVersion(pkg = package_name))
-  
+
   cat(
     "Suffolk County Council Rmd template package",
     paste0("\"", package_name, "\""),
@@ -28,7 +35,6 @@ print_version <- function() {
     paste0("(", date, ")."),
     fill = TRUE
   )
-  
+
   invisible(NULL)
-  
 }
